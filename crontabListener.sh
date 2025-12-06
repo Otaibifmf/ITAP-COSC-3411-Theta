e6e5eafa67173ce627a1aa287369d03a7f3a115a
sackboy <118112953+DrSackboy@users.noreply.github.com>
2025-12-06 09:19:00 +0300

    Attacker based scheduled crontab

--- crontabListener.sh
+++ crontabListener.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
 read -p "Enter port you would like to listen to? " PORT
-
+read -p "Minute (0-59): " MIN
+read -p "Hour: (0-23) " HOUR
+read -p "Day of Month: (1-31) " DOM
+read -p "Month: (1-12) " MON
+read -p "Day of Week: (0-6) " DOW
 
 COMMAND="nc -lnvp $PORT"
 
-CRONLINE="0 17 * * * $COMMAND"
+CRONLINE="$MIN $HOUR $DOM $MON $DOW $COMMAND"
 
 ( crontab -l 2>/dev/null | grep -v 'nc -lnvp' ; echo "$CRONLINE" ) | crontab -
 
